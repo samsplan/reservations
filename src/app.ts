6ca7fedb4be6b26d7e42ae9ec571207e949e8665
Sam <samrtomkinson@gmail.com>
2018-07-01 22:58:46 +0100

    Added swagger generation using tsoa

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import RegisterRoutes from './routes/routes';
+import swaggerUi from 'swagger-ui-express';
+
+import swaggerDocument from './swagger/swagger.json';
 
 // Create Express server
 const app = express();
 
 // Express configuration
 app.set('port', process.env.PORT || 3000);
+
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/swagger.json', express.static(`${__dirname}/swagger/swagger.json`));
 
 // Route definitions
 RegisterRoutes(app);
